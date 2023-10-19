c6ad80b70e1de8085750a47b06c791a86990ca4a
Yibrah Teare <112482941+yobocode@users.noreply.github.com>
2023-10-19 14:34:21 +0300

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,27 @@
 #!/usr/bin/python3
 
-import sys
+from sys import stdin
 
 
-def print_msg(dict_sc, total_file_size):
-    """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
-    """
-
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
-
-
-total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+size = 0
+code = {"200": 0, "301": 0, "400": 0, "401": 0,
+        "403": 0, "404": 0, "405": 0, "500": 0}
 
 try:
-    for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
-
-        if len(parsed_line) > 2:
-            counter += 1
-
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])  # file size
-                code = parsed_line[1]  # status code
-
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
-
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
-
+    for i, line in enumerate(stdin, 1):
+        split = line.split(" ")
+        if len(split) < 2:
+            continue
+        if split[-2] in code:
+            code[split[-2]] = code[split[-2]] + 1
+        size = size + eval(split[-1])
+        if i % 10 == 0:
+            print("File size: {}".format(size))
+            for key in sorted(code.keys()):
+                if code[key] > 0:
+                    print("{}: {}".format(key, code[key]))
 finally:
-    print_msg(dict_sc, total_file_size)
+    print("File size: {}".format(size))
+    for key in sorted(code.keys()):
+        if code[key] > 0:
+            print("{}: {}".format(key, code[key]))
